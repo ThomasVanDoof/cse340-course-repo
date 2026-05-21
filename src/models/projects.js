b44e34f51257fd2d9d1f53a9bc708fc6ca9be088
Cave Smith <dracothedragon9292@gmail.com>
2026-05-21 14:41:42 -0600

    added the organization details page

--- src/models/projects.js
+++ src/models/projects.js
@@ -7,5 +7,24 @@ const getAllProjects = async() => {
   const result = await db.query(query);
   return result.rows;
 }
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          project_id,
+          organization_id,
+          name,
+          description,
+          location,
+          date
+        FROM service_projects
+        WHERE organization_id = $1
+        ORDER BY date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
 
-export { getAllProjects }
+      return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId }
