ebf9c27dbe2d6a76fb36e86ab9bc6f4c5d03cb2a
Cave Smith <dracothedragon9292@gmail.com>
2026-05-23 14:54:22 -0600

    adding project details

--- src/models/projects.js
+++ src/models/projects.js
@@ -27,4 +27,48 @@ const getProjectsByOrganizationId = async (organizationId) => {
       return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId }
+const getUpcomingProjects = async (number_of_projects) => {
+  const query = `
+    SELECT
+      p.project_id,
+      p.name AS title,
+      p.description,
+      p.date,
+      p.location,
+      p.organization_id,
+      o.name AS organization_name
+    FROM service_projects p
+    JOIN organizations o ON p.organization_id = o.organization_id
+    WHERE p.date::DATE >= CURRENT_DATE
+    ORDER BY p.date ASC
+    LIMIT $1
+  `;
+  
+  const queryParams = [number_of_projects];
+  const result = await db.query(query, queryParams);
+  
+  return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+  const query = `
+    SELECT
+      p.project_id,
+      p.name AS title,
+      p.description,
+      p.date,
+      p.location,
+      p.organization_id,
+      o.name AS organization_name
+    FROM service_projects p
+    JOIN organizations o ON p.organization_id = o.organization_id
+    WHERE p.project_id = $1
+  `;
+  
+  const queryParams = [id];
+  const result = await db.query(query, queryParams);
+  
+  return result.rows[0];
+};
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails }
